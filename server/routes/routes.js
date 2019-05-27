ef711b2b5f67ab3d9d5b1ae8323e76b7664de71f
Keyboard Jedi <46779011+KeyboardJedi@users.noreply.github.com>
2019-05-27 13:21:44 +0200

    awd

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,5 +1,17 @@
 module.exports = (app) => {
-   app.get('/', (req, res, next) => {
+   app.get('/home', (req, res, next) => {
       res.render('home');
    });
+   app.get('/product', (req, res, next) => {
+      res.render('product');
+   });
+   app.get('/contact', (req, res, next) => {
+      res.render('contact');
+   });
+   app.get('/test', (req, res, next) => {
+      res.render('test');
+      // res.send();
+      // console.log("Hej")
+   });
 };
+
